d1cf442f4593463f53675928f913bdb72b4394e1
Luiz Roberto Silva <robertolg.luiz@gmail.com>
2018-02-25 17:11:05 -0300

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -16,18 +16,38 @@ app.use(express.static(publicPatch));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    })
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    })
+
+
+
+
     socket.on('disconnect', (socket) => {
         console.log('User disconnected');
     });
 
     socket.on('createMessage', (message) => {
         console.log('Createmessage', message);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
-    })
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+    });
 });
 
 server.listen(port, () => {
