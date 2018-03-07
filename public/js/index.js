39fb87512835148f068256d5816495da8b71518c
Luiz Silva <luiz.moreira@ventron.com.br>
2018-03-07 13:12:31 -0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,27 +33,31 @@ socket.on('newLocationMessage', function (message) {
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
+
+    var messageTextBox = $('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextBox.val()
     }, function () {
-
+        messageTextBox.val('');
     })
 });
 
 var locationBtn = $('#sendGeoLocation');
-
 locationBtn.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geo Location not suported by your browser');
     }
+    locationBtn.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function (position) {
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
-
+        locationBtn.removeAttr('disabled').text('Send location');
     }, function (err) {
+        locationBtn.removeAttr('disabled').text('Send location');
         alert('Unable to fetch position');
     });
 
